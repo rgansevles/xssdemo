b13e87f41f959fd69fd8e7de5aead80ed33ddeec
Rob Gansevles <rgansevles@gmail.com>
2017-01-22 13:14:57 +0100

    Content-Security-Policy

--- src/main/java/rob/xss/XssDemo.java
+++ src/main/java/rob/xss/XssDemo.java
@@ -1,14 +1,13 @@
 package rob.xss;
-import static spark.Spark.get;
-
-import org.owasp.html.PolicyFactory;
-import org.owasp.html.Sanitizers;
-
+import static spark.Spark.*;
 
 public class XssDemo {
 
 	public static void main(String[] args) {
-		get("/xssdemo", (req, res) -> "<html></body>"+getMessage(args)+"<html></body>");
+		get("/xssdemo", (req, res) -> {
+			res.header("Content-Security-Policy", "script-src 'self'");
+			return "<html></body>"+getMessage(args)+"<html></body>";
+		});
 	}
 
 	private static String getMessage(String[] args) {
@@ -16,8 +15,7 @@ public class XssDemo {
 			return "??";
 		}
 		
-		PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
-		return policy.sanitize(args[0]);
+		return args[0];
 	}
 
 }
