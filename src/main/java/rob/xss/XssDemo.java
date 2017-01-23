9d69d28f8ec0d61a91695a38c37453a4fd1a1ed0
Rob Gansevles <rgansevles@gmail.com>
2017-01-23 20:41:07 +0100

    encode-html

--- src/main/java/rob/xss/XssDemo.java
+++ src/main/java/rob/xss/XssDemo.java
@@ -1,21 +1,20 @@
 package rob.xss;
-import static spark.Spark.*;
+import static spark.Spark.get;
+
+import org.owasp.encoder.Encode;
 
 public class XssDemo {
 
 	public static void main(String[] args) {
-		get("/xssdemo", (req, res) -> {
-			res.header("Content-Security-Policy", "script-src 'self'");
-			return "<html></body>"+getMessage(args)+"<html></body>";
-		});
+		get("/xssdemo", (req, res) -> "<html></body>"+getMessage(args)+"<html></body>");
 	}
 
 	private static String getMessage(String[] args) {
 		if (args == null || args.length == 0) {
 			return "??";
 		}
-		
-		return args[0];
+
+		return Encode.forHtml(args[0]);
 	}
 
 }
